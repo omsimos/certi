67770bb0f66702f2b850b701b685f3bfb91695df
hyamero <daleban.dev@gmail.com>
2024-04-09 12:14:53 +0800

    feat: route to event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import { EventForm } from "@/components/event-form";
 import { ToggleTheme } from "@/components/toggle-theme";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-40 p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Certificate Generator&nbsp;
+          <code className="font-mono font-bold">by hyamero</code>
         </p>
-        <div className="flex gap-2 items-center">
-          by hyamero
-          <ToggleTheme />
-        </div>
+        <ToggleTheme />
+      </div>
+
+      <div>
+        <h3 className="text-lg mb-2 font-semibold">Search Event</h3>
+        <EventForm />
       </div>
     </main>
   );
---
+++ src/components/event-form.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { type FormEvent, useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+export function EventForm() {
+  const router = useRouter();
+  const [event, setEvent] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/event?name=${event}`);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      className="flex w-full max-w-sm items-center space-x-2"
+    >
+      <Input
+        type="text"
+        placeholder="Event"
+        value={event}
+        onChange={(e) => setEvent(e.target.value)}
+      />
+      <Button type="submit">Visit</Button>
+    </form>
+  );
+}
