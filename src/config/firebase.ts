5a41f796b6632cc12610440e4da9146206f5eed1
hyamero <daleban.dev@gmail.com>
2024-04-10 15:58:10 +0800

    fix: update firebase analytics when window is defined

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,25 @@
 import { initializeApp } from "firebase/app";
+import { Analytics, getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+
+// eslint-disable-next-line import/no-mutable-exports
+let analytics: Analytics;
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID!,
 };
 
-// Initialize Firebase
-let app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
+
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
 
 export { app, analytics, db };
