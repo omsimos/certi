e68d505bb7e1594c33f8b48947f0e21128cde6d3
hyamero <daleban.dev@gmail.com>
2024-04-11 16:18:26 +0800

    chore: temp loading pages

---
+++ src/app/cert/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="h-screen flex items-center pt-40 gap-10 flex-col">
+      <div className="text-center">
+        <h1 className="capitalize font-bold text-6xl">Loading...</h1>
+      </div>
+    </main>
+  );
+}
