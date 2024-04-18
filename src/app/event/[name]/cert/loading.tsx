d53ea092dfb3f4f2db5ed649980f75b478958f97
hyamero <daleban.dev@gmail.com>
2024-04-18 13:09:45 +0800

    feat: icons component

--- src/app/event/[name]/cert/loading.tsx
+++ src/app/event/[name]/cert/loading.tsx
@@ -1,9 +1,9 @@
+import { Icons } from "@/components/icons";
+
 export default function Loading() {
   return (
-    <main className="h-screen flex items-center pt-40 gap-10 flex-col">
-      <div className="text-center">
-        <h1 className="capitalize font-bold text-6xl">Loading...</h1>
-      </div>
+    <main className="h-screen flex items-center justify-center gap-10 flex-col">
+      <Icons.spinner />
     </main>
   );
 }
