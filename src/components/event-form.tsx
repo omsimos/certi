ca57aef06deed0f2a1ca9e44ea9571fb90c6a359
hyamero <daleban.dev@gmail.com>
2024-04-21 19:52:07 +0800

    feat: responsive dialog to drawer event form

--- src/components/event-form.tsx
+++ src/components/event-form.tsx
@@ -7,14 +7,76 @@ import { useRouter } from "next/navigation";
 
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
   DrawerHeader,
   DrawerTitle,
 } from "@/components/ui/drawer";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import useMediaQuery from "@/hooks/use-media-query";
+
 export function EventForm() {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <>
+        <div className="flex gap-3 [&>*]:px-10">
+          <Button onClick={() => setOpen(!open)}>Find Event</Button>
+          <Button variant="secondary">Setup Event</Button>
+        </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Find Event</DialogTitle>
+              <DialogDescription>Enter the event code</DialogDescription>
+            </DialogHeader>
+            <SearchEventForm openState={open} setOpenState={setOpen} />
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex gap-3 [&>*]:px-10">
+        <Button onClick={() => setOpen(!open)}>Find Event</Button>
+        <Button variant="secondary">Setup Event</Button>
+      </div>
+
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerContent>
+          <div className="pt-7 pb-36 max-w-sm w-full mx-auto">
+            <DrawerHeader className="px-0">
+              <DrawerTitle>Find Event</DrawerTitle>
+              <DrawerDescription>Enter the event code</DrawerDescription>
+            </DrawerHeader>
+            <SearchEventForm openState={open} setOpenState={setOpen} />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+
+const SearchEventForm = ({
+  openState,
+  setOpenState,
+}: {
+  openState: boolean;
+  setOpenState: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   const router = useRouter();
   const [event, setEvent] = useState("");
 
@@ -22,47 +84,32 @@ export function EventForm() {
     e.preventDefault();
     router.push(`/event/${event}`);
   };
-
-  const [openDrawer, setOpenDrawer] = useState(false);
-
   return (
-    <div>
-      <div className="flex gap-3 [&>*]:px-10">
-        <Button onClick={() => setOpenDrawer(!openDrawer)}>Find Event</Button>
-        <Button variant="secondary">Setup Event</Button>
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      className="flex w-full max-w-sm items-center gap-2 flex-col"
+    >
+      <Input
+        type="text"
+        placeholder="Event"
+        value={event}
+        onChange={(e) => setEvent(e.target.value)}
+      />
+      <div className="flex flex-col gap-2 w-full">
+        <Button type="submit" className="w-full">
+          Search
+        </Button>
+        <Button
+          variant="outline"
+          onClick={(e) => {
+            e.preventDefault();
+            setOpenState(!openState);
+          }}
+          className="w-full"
+        >
+          Cancel
+        </Button>
       </div>
-
-      <Drawer open={openDrawer} onOpenChange={setOpenDrawer}>
-        <DrawerContent>
-          <div className="pt-7 pb-36 max-w-sm w-full mx-auto">
-            <DrawerHeader className="px-0">
-              <DrawerTitle>Find Event</DrawerTitle>
-              <DrawerDescription>Enter event code</DrawerDescription>
-            </DrawerHeader>
-            <form
-              onSubmit={(e) => handleSubmit(e)}
-              className="flex w-full max-w-sm items-center gap-2 flex-col"
-            >
-              <Input
-                type="text"
-                placeholder="Event"
-                value={event}
-                onChange={(e) => setEvent(e.target.value)}
-              />
-              <div className="flex flex-col gap-2 w-full">
-                <Button type="submit" className="w-full">
-                  Search
-                </Button>
-                <DrawerClose className="w-full">
-                  <Button variant="outline" className="w-full">
-                    Cancel
-                  </Button>
-                </DrawerClose>
-              </div>
-            </form>
-          </div>
-        </DrawerContent>
-      </Drawer>
-    </div>
+    </form>
   );
-}
+};
