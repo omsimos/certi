ab243c4802ffc7e2830c0c1fdb68f2443812fb5f
hyamero <daleban.dev@gmail.com>
2024-04-30 12:50:19 +0800

    feat: event setup with form validation

---
+++ src/app/event-setup/components/add-event-form.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "sonner";
+
+const profileFormSchema = z.object({
+  eventCode: z
+    .string()
+    .min(2, {
+      message: "Event code must be at least 5 characters.",
+    })
+    .max(16, {
+      message: "Event code must not be longer than 16 characters.",
+    })
+    .refine((s) => !s.includes(" "), "No Spaces!"),
+  title: z
+    .string()
+    .min(2, {
+      message: "Title must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Title must not be longer than 30 characters.",
+    }),
+  description: z
+    .string()
+    .min(25, {
+      message: "Description must be at least 25 characters.",
+    })
+    .max(250, {
+      message: "Description must not be longer than 250 characters.",
+    }),
+  organizer: z
+    .string()
+    .min(2, {
+      message: "Organizer must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Organizer must not be longer than 30 characters.",
+    }),
+});
+
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<ProfileFormValues> = {};
+
+export function AddEventForm() {
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  function onSubmit(data: ProfileFormValues) {
+    toast("You submitted the following values", {
+      description: JSON.stringify(data, null, 2),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="eventCode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event Code</FormLabel>
+              <FormControl>
+                <Input placeholder="omsimos-os" {...field} />
+              </FormControl>
+              <FormDescription>
+                Event code used to search for an event
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Omsimos: The Collective OS" {...field} />
+              </FormControl>
+              <FormDescription>Title of the event</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us about it!"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Description of the event</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="organizer"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Organizer</FormLabel>
+              <FormControl>
+                <Input placeholder="omsimos" {...field} />
+              </FormControl>
+              <FormDescription>Event organizer</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add Event</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/event-setup/page.tsx
@@ -0,0 +1,19 @@
+import { Separator } from "@/components/ui/separator";
+import { AddEventForm } from "./components/add-event-form";
+
+export default function EventSetup() {
+  return (
+    <main className="container flex min-h-screen flex-col gap-5 pb-20 pt-36 sm:items-center">
+      <div className="w-full max-w-md space-y-6">
+        <div>
+          <h3 className="text-lg font-medium">Setup Event</h3>
+          <p className="text-sm text-muted-foreground">
+            Generate a certificate generator for your event.
+          </p>
+        </div>
+        <Separator />
+        <AddEventForm />
+      </div>
+    </main>
+  );
+}
