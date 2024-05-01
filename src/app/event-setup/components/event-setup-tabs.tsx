2637feb573f998e2707a060e6cd9d19012a5755e
hyamero <daleban.dev@gmail.com>
2024-05-01 16:51:25 +0800

    feat: tab url search params

--- src/app/event-setup/components/event-setup-tabs.tsx
+++ src/app/event-setup/components/event-setup-tabs.tsx
@@ -1,15 +1,32 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { AddEventForm } from "./add-event-form";
 import AddParticipants from "./add-participants";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function EventSetupTabs() {
+  const searchParams = useSearchParams();
+  const currentTab = searchParams.get("tab");
+
+  function updateTab(tab: string) {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("tab", tab);
+    window.history.pushState(null, "", `?${params.toString()}`);
+  }
+
   return (
-    <Tabs defaultValue="participants" className="w-full max-w-md">
+    <Tabs defaultValue={currentTab!} className="w-full max-w-md">
       <TabsList className="mb-10 w-full [&>*]:w-full">
-        <TabsTrigger value="participants">Participants</TabsTrigger>
-        <TabsTrigger value="details">Details</TabsTrigger>
+        <TabsTrigger
+          onClick={() => updateTab("participants")}
+          value="participants"
+        >
+          Participants
+        </TabsTrigger>
+        <TabsTrigger onClick={() => updateTab("details")} value="details">
+          Details
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="participants">
         <AddParticipants />
