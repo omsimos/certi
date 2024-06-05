548450dde206a2b30f4680b3fe1a5f1d6334cd9a
hyamero <daleban.dev@gmail.com>
2024-06-05 13:33:22 +0800

    chore: add arrow icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,24 @@
 type Props = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
+  arrowLeft: (props: Props) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+      />
+    </svg>
+  ),
+
   wrench: (props: Props) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
