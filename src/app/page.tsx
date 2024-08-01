f839e3454089c2937c7c68f0770b900931c6d5a6
Dale B <daleban.dev@gmail.com>
2024-08-01 23:55:11 +0800

    feat: add animated shiny button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import { EventButtons } from "@/components/event-buttons";
+import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
+import { cn } from "@/lib/utils";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <main className="container flex min-h-screen flex-col items-center gap-6 py-36 md:gap-8 lg:pt-64">
-      <div className="border-b-2 border-dashed border-muted pb-6 md:pb-8">
+      <div className="flex flex-col items-center gap-2 border-b-2 border-dashed border-muted pb-6 md:pb-8">
+        <div
+          className={cn(
+            "bg-bg group rounded-full border border-black/5 text-base text-foreground transition-all ease-in hover:cursor-pointer hover:bg-slate-900 dark:border-white/5",
+          )}
+        >
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+            <Link
+              href="https://github.com/hyamero/certificate-generator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ✨ Contribute to GitHub
+            </Link>
+            <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+          </AnimatedShinyText>
+        </div>
+
         <h1 className="bg-zinc-800 from-foreground bg-clip-text text-center text-[10vw] font-extrabold leading-none tracking-tighter text-transparent dark:bg-gradient-to-b dark:to-zinc-400 md:text-7xl">
           Effortlessly create and distribute e-certificates for your events
         </h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +67,23 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        shimmer: {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shimmer-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shimmer-width)) 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: "shimmer 8s infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
