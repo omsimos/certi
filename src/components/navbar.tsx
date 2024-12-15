e7b5d7bd41eff05e35c471c7b54e36345ff23b13
Joseph Dale Banares <daleban.dev@gmail.com>
2024-12-15 12:59:45 +0800

    chore: update nav url to omsimos

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,9 +5,13 @@ import ManageEventDialog from "./manage-event-dialog";
 export function Navbar() {
   return (
     <div className="container absolute left-0 right-0 top-0 z-10 mx-auto flex items-center justify-between pt-10 font-mono text-sm">
-      <Link href="/">
-        Certi&nbsp;
-        <code className="font-mono font-bold">by hyamero</code>
+      <Link
+        href="https://www.facebook.com/omsimos.official"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Certi by&nbsp;
+        <code className="font-mono font-bold">Omsimos</code>
       </Link>
       <div className="space-x-2">
         <ToggleTheme />
